91f8e3bb6a63f4af4b13b9a4caa32827fa0eb8b8
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-15 11:40:21 -0500

    :sparkles: tuples

---
+++ rust_by_example/02_primitives/03_tuples.rs
@@ -0,0 +1,44 @@
+/*
+* A tuple is a collection of values of different types.
+* Tuples are constructed using parentheses (), and each
+* tuple itself is a value with type signature (T1, T2, ...),
+* where T1, T2 are the types of its members.
+* Functions can use tuples to return multiple values,
+* as tuples can hold any number of values.
+*/
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn main() {
+    let long_tuple = (
+        1, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple second value: {}", long_tuple.9);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    println!("one element tuple: {:?}", (5u32,));
+    println!("just an integer: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, true);
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+}
