3368c35592cdc1f903035dfa5f82c71fafd980cf
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-22 09:39:16 -0500

    :sparkles: Update

--- book/04_ownership/src/len.rs
+++ book/04_ownership/src/len.rs
@@ -1,9 +1,7 @@
 fn main() {
     let s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
-
-    println!("The length of '{}' is {}.", s1, len);
+    println!("The length of '{}' is {}.", s1, calculate_length(&s1));
 }
 
 fn calculate_length(s: &String) -> usize {
