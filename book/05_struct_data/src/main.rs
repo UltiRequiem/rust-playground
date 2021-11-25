bbdac9fb7f7f166b976753da244fd3810b459e3c
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2021-11-24 21:46:50 -0500

    :sparkles: Structs

--- book/05_struct_data/src/main.rs
+++ book/05_struct_data/src/main.rs
@@ -1,3 +1,29 @@
+use std::fmt;
+
+struct Person {
+    name: String,
+    age: usize,
+}
+
+impl fmt::Display for Person {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} is {} years old.", self.name, self.age)
+    }
+}
+
+impl Person {
+    fn say_hi(&self) {
+        println!("Hi, I'm {} and {} years old.", self.name, self.age);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let diah = &Person {
+        name: String::from("Diah"),
+        age: 15,
+    };
+
+    println!("{}", diah);
+
+    diah.say_hi();
 }
